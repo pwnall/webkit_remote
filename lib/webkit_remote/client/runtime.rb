7ae51f86ee00a411ffe8d66fc1482362b0c9535c
Victor Costan <costan@gmail.com>
2012-10-22 21:03:53 -0400

    Work on the Runtime domain.

---
+++ lib/webkit_remote/client/runtime.rb
@@ -0,0 +1,31 @@
+module WebkitRemote
+
+class Client
+
+# API for the Runtime domain.
+module Runtime
+  # Evals a JavaScript expression.
+  #
+  # @param [String] expression the JavaScript expression to be evaluated
+  # @param [String, Symbol] group the name of an object group (think memory
+  #     pools); the objects in a group can be released together in a single
+  #     call
+  # @return [WebkitRemote::Client::RemoteObject] self
+  def evaluate()
+    # NOTE: returnByValue is always set to true to avoid some extra complexity
+    raw_object = @rpc.call 'Runtime.evaluate', script: script,
+        objectGroup: group || '_', returnByValue: true
+    WebkitRemote::Client::RemoteObject.new raw_object, self
+  end
+end  # module WebkitRemote::Client::Runtime
+
+# Mirrors a RemoteObject, defined in the Runtime domain.
+class RemoteObject
+  def initialize(raw_object)
+
+  end
+end  # class WebkitRemote::Client::RemoteObject
+
+end  # namespace WebkitRemote::Client
+
+end  # namespace WebkitRemote
