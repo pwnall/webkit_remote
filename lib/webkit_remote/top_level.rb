14c0763a7bf9b7fd89a9137926fb2b509e9d75c4
Victor Costan <costan@gmail.com>
2012-10-29 18:17:45 -0400

    Some documentation in preparation for a release.

--- lib/webkit_remote/top_level.rb
+++ lib/webkit_remote/top_level.rb
@@ -2,6 +2,7 @@
 module WebkitRemote
   # Launches a WebKit process locally, and sets up a debugger client for it.
   #
+  # @param (see WebkitRemote::Process#initialize)
   # @return [WebkitRemote::Client] a debugging client connected to a local
   #     WebKit process; the client will automatically stop the process when
   #     closed
@@ -10,6 +11,20 @@ module WebkitRemote
     browser = process.start
     browser.stop_process = true
     client = WebkitRemote::Client.new tab: browser.tabs.first,
+                                      close_browser: true
+    client
+  end
+
+  # Connects to a Webkit process, and sets up a debugger client for it.
+  #
+  # @param (see WebkitRemote::Browser#initialize)
+  # @return [WebkitRemote::Client] a debugging client connected to the remote
+  #     WebKit process; the connection will be automatically terminated when
+  #     the debugging client is closed
+  def self.remote(opts = {})
+    browser = WebkitRemote::Browser.new opts
+    # NOTE: connecting to the last tab to avoid internal tabs and whatnot
+    client = WebkitRemote::Client.new tab: browser.tabs.last,
                                       close_browser: true
     client
   end
--- test/webkit_remote_test.rb
+++ test/webkit_remote_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('helper.rb', File.dirname(__FILE__))
 
 describe WebkitRemote do
-  describe '#local' do
+  describe 'local' do
     before do
       @client = WebkitRemote.local port: 9669
     end
@@ -23,6 +23,40 @@ describe WebkitRemote do
         @client.closed?.must_equal true
         @client.browser.closed?.must_equal true
         @client.browser.process.running?.must_equal false
+      end
+    end
+  end
+
+  describe 'remote' do
+    before do
+      @process = WebkitRemote::Process.new port: 9669
+      browser = @process.start
+      browser.close
+      @client = WebkitRemote.remote host: 'localhost', port: 9669
+    end
+
+    after do
+      @client.close
+      @process.stop
+    end
+
+    it 'returns a working client' do
+      @client.must_be_kind_of WebkitRemote::Client
+      @client.closed?.must_equal false
+    end
+
+    describe 'after #close' do
+      before do
+        @client.close
+      end
+
+      it 'the client tears the connection' do
+        @client.closed?.must_equal true
+        @client.browser.closed?.must_equal true
+      end
+
+      it 'the client does not impact the browser process' do
+        @process.running?.must_equal true
       end
     end
   end
